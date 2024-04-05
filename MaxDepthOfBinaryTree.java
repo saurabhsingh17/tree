81666baa5e462dfd0ef817ff1d1508dc55fa522e
saurabhsingh <saurabh623a@gmail.com>
2024-04-05 23:13:34 +0530

    Day-1 Tree Basics

---
+++ MaxDepthOfBinaryTree.java
@@ -0,0 +1,8 @@
+public class MaxDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return 1 + Math.max(lh,rh);
+    }
+}
